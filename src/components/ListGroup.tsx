f7608d4fa1e1594cdd56b2b8dd16b388684e7d8b
Vincent Haguet <vincent.haguet@malt.com>
2025-06-04 16:44:50 +0200

    Rendering lists

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,14 @@
 function ListGroup() {
+  const items = ["New York", "San Fransisco", "Tokyo", "London"];
   return (
     <>
       <h1>List </h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
