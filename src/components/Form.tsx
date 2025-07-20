629a6432cdfd851b016a622bff2ac7b68b6c3835
Vincent Haguet <vincent.haguet@malt.com>
2025-07-20 22:15:59 +0200

    Applying validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,17 @@
 import React, { FormEvent, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 
+interface FormData {
+  age: number;
+  name: string;
+}
+
 const Form = () => {
-  const { register, handleSubmit } = useForm();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
   const onSubmit = (data: FieldValues) => console.log(data);
 
   return (
@@ -12,11 +21,19 @@ const Form = () => {
           Name
         </label>
         <input
-          {...register("name")}
+          {...register("name", { required: true, minLength: 3 })}
           id="name"
           type="text"
           className="form-control"
         />
+        {errors.name?.type === "required" && (
+          <p className="text-danger"> The name field is required</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger ">
+            The name field must be at least 3 characters
+          </p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
