e0494a710f963eda904273f0eafd62c1d3c03f3f
Vincent Haguet <vincent.haguet@malt.com>
2025-07-31 15:33:03 +0200

    Create ExpenseList component

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,21 @@ import Button from "./components/Button/Button";
 import DismissingAlert from "./components/DismissingAlert";
 import Like from "./components/Like";
 import Form from "./components/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import { Expense } from "./expense-tracker/components/ExpenseList";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "Burger", amount: 10, category: "Food" },
+    { id: 2, description: "Phone", amount: 10, category: "Tech" },
+    { id: 3, description: "Table", amount: 10, category: "Home" },
+  ]);
+  const onDelete = (id: number) => {
+    setExpenses(expenses.filter((expense) => expense.id !== id));
+  };
   return (
     <div>
-      <Form />
+      <ExpenseList expenses={expenses} onDelete={onDelete} />
     </div>
   );
 }
